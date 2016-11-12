9d9653c06632cea22b3126bb40c34d57185156b7
Saul <digitalh2o@gmail.com>
2016-11-12 17:05:16 -0600

    search can now be filter and comes ascending order

--- app/assets/javascripts/angular/controllers/SearchController.js
+++ app/assets/javascripts/angular/controllers/SearchController.js
@@ -4,7 +4,7 @@ function SearchController($scope, $http, GetGameService){
 
 
   $scope.getGames = function (){
-    return $http.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Crating%2Ccover%2Curl%2Csummary%2Cfirst_release_date&limit=50&offset=0&order=release_dates.date%3Adesc&search=" + $scope.search, {"headers": {
+    return $http.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Crating%2Ccover%2Curl%2Csummary%2Cfirst_release_date&limit=50&offset=0&order=release_dates.date%3Aasc&search=" + $scope.search, {"headers": {
         "x-mashape-key": "fwLQAtGKfGmshnRQbe2WhlZJX45Cp1XKznUjsn09VXCW6ZCI8J",
          "accept": "application/json",
        }
